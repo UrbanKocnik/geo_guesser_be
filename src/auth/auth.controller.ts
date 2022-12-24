c647f099f8e6f04d5b2d35963bb528f80b3d179e
Urban <urban.kocnik@gmail.com>
2022-12-24 14:09:05 +0100

    added swagger
    1. installed swagger
    2.  added swagger options and route to main.ts
    3. Dodal v dto api property da jih prepozna
    4. Routi zaznani avtomatsko
    5. Pred tiste ko rabijo prijavo mores posebi dat @ApiBearerAuth
    6. Pred auth GET type route mors se data @SerializeOptions da expose providerja

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, HttpCode, HttpStatus, Patch, Post, UseGuards, UseInterceptors, Request, Get } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, HttpCode, HttpStatus, Patch, Post, UseGuards, UseInterceptors, Request, Get, SerializeOptions } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthEmailLoginDto } from './dto/auth-email-login.dto';
 import { AuthRegisterLoginDto } from './dto/auth-register-login.dto';
-import { AuthGuard } from '@nestjs/passport';
-import { AuthUpdateDto } from './dto/auth-update.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { AuthResetPasswordDto } from './dto/auth-reset-password.dto';
 import { AuthForgotPasswordDto } from './dto/auth-forgot-password.dto';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('auth')
@@ -23,29 +23,8 @@ export class AuthController {
     @HttpCode(HttpStatus.CREATED)
     async register(@Body() createUserDto: AuthRegisterLoginDto) {
       return this.service.register(createUserDto);
-    }               
-
-    @Get('me')
-    @UseGuards(JwtAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    public async me(@Request() request) {
-      return this.service.me(request.user);
-    }
+    }       
     
-    @Patch('me')
-    @UseGuards(JwtAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    public async update(@Request() request, @Body() userDto: AuthUpdateDto) {
-      return this.service.update(request.user, userDto);
-    }
-
-    @Delete('me')
-    @UseGuards(JwtAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    public async delete(@Request() request) {
-      return this.service.delete(request.user);
-    }
-
     @Post('forgot/password')
     @HttpCode(HttpStatus.OK)
     async forgotPassword(@Body() forgotPasswordDto: AuthForgotPasswordDto) {
@@ -60,4 +39,33 @@ export class AuthController {
         resetPasswordDto.password,
       );
     }
+
+    @ApiBearerAuth()
+    @SerializeOptions({
+    groups: ['exposeProvider'],
+    })  
+    @Get('me')
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    public async me(@Request() request) {
+      return this.service.me(request.user);
+    }
+    
+    @ApiBearerAuth()
+    @Patch('me')
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    public async update(@Request() request, @Body() userDto: UpdateUserDto) {
+      return this.service.update(request.user, userDto);
+    }
+
+    @ApiBearerAuth()
+    @Delete('me')
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    public async delete(@Request() request) {
+      return this.service.delete(request.user);
+    }
+
+
   }
--- src/auth/dto/auth-email-login.dto.ts
+++ src/auth/dto/auth-email-login.dto.ts
@@ -1,11 +1,14 @@
 
 import { IsNotEmpty, Validate } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthEmailLoginDto {
+  @ApiProperty({ example: 'test1@example.com' })
   @Transform(({ value }) => value.toLowerCase().trim())
   email: string;
-
+  
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
