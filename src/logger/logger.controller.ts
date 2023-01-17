c26f0fd1cc2c2cc6ba2a1decb0ecd7c94c533e65
Urban <urban.kocnik@gmail.com>
2023-01-17 14:46:47 +0100

    logger
    added logger module, service and controller,
    added logger table and logger dtos,
    added logger service methods that adds logger entries, and if the user is admin with pagination fetches last 100 log entries

---
+++ src/logger/logger.controller.ts
@@ -0,0 +1,47 @@
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Request,
+  Post,
+  Query,
+  SerializeOptions,
+  UseGuards,
+  UseInterceptors,
+  Body,
+} from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { TransformInterceptor } from 'src/common/response.interceptor';
+import { CreateLogArrayDto } from './dto/create-log-array.dto copy';
+import { LoggerService } from './logger.service';
+
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(JwtAuthGuard)
+@UseInterceptors(TransformInterceptor)
+@Controller('logger')
+export class LoggerController {
+  constructor(private loggerService: LoggerService) {}
+
+  @ApiBearerAuth()
+  @SerializeOptions({
+    groups: ['exposeProvider'],
+  })
+  @Get('all')
+  async all(
+    @Request() request,
+    @Query('page') page = 1,
+    @Query('take') take = 100,
+    @Query('condition') condition = 'createdAt',
+  ) {
+    console.log('here');
+    return this.loggerService.getLogs(page, take, condition, request.user);
+  }
+
+  @ApiBearerAuth()
+  @Post('add')
+  async create(@Request() request, @Body() loggerDto: CreateLogArrayDto) {
+    console.log(loggerDto);
+    return this.loggerService.createLogEntries(request.user, loggerDto);
+  }
+}
